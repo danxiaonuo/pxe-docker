135cdee7d2cfcc397830cf79642fbac00bc69780
danxiaonuo <danxiaonuo@danxiaonuo.me>
2023-09-27 09:32:20 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,57 @@
 #!/bin/bash
 
 # 导入环境变量
-export 'PXE_IP='$(for e in $(ls -l /sys/class/net/ | grep -v virtual  | grep -v 'total' | awk '{print $9}'); do ifconfig | grep -1 $e | grep inet | awk '{print $2}';done)
-export 'INTERFACE='$(ls -l /sys/class/net/ | grep -v virtual  | grep -v 'total' | awk '{print $9}')''
-export 'DHCP_RANGE_LOW='$(for e in $(ls -l /sys/class/net/ | grep -v virtual  | grep -v 'total' | awk '{print $9}'); do ifconfig | grep -1 $e | grep inet | awk '{print $2}';done | awk 'BEGIN{ FS = "."} {print $1"."$2"."$3".10"}')
-export 'DHCP_RANGE_HIGH='$(for e in $(ls -l /sys/class/net/ | grep -v virtual  | grep -v 'total' | awk '{print $9}'); do ifconfig | grep -1 $e | grep inet | awk '{print $2}';done | awk 'BEGIN{ FS = "."} {print $1"."$2"."$3".245"}')
+# PXE IP地址
+PXE_IP=$(for e in $(ls -l /sys/class/net/ | grep -v virtual  | grep -v 'total' | awk '{print $9}'); do ifconfig | grep -1 $e | grep inet | awk '{print $2}';done)
+# 接口名称
+INTERFACE=$(ls -l /sys/class/net/ | grep -v virtual  | grep -v 'total' | awk '{print $9}')
+# DHCP起始地址
+DHCP_RANGE_LOW=$(for e in $(ls -l /sys/class/net/ | grep -v virtual  | grep -v 'total' | awk '{print $9}'); do ifconfig | grep -1 $e | grep inet | awk '{print $2}';done | awk 'BEGIN{ FS = "."} {print $1"."$2"."$3".10"}')
+# DHCP起始结束地址
+DHCP_RANGE_HIGH=$(for e in $(ls -l /sys/class/net/ | grep -v virtual  | grep -v 'total' | awk '{print $9}'); do ifconfig | grep -1 $e | grep inet | awk '{print $2}';done | awk 'BEGIN{ FS = "."} {print $1"."$2"."$3".245"}')
+# 网关地址
+GATEWAY_IP=$(ip route | grep -i 'via' |awk 'END {print $3}')
 
+# 环境变量
+# 接口名称
+INTERFACE="${INTERFACE:=${INTERFACE}}"
+# PXE IP地址
+PXE_IP="${PXE_IP:=${PXE_IP}}"
+# DHCP起始地址
+DHCP_RANGE_LOW="${DHCP_RANGE_LOW:=${DHCP_RANGE_LOW}}"
+# DHCP起始结束地址
+DHCP_RANGE_HIGH="${DHCP_RANGE_HIGH:=${DHCP_RANGE_HIGH}}"
+# 网关地址
+GATEWAY_IP="${GATEWAY_IP:=${GATEWAY_IP}}"
+# DHCP DNS地址
+DHCP_DNS="${DHCP_DNS:=223.5.5.5}"
+# DHCP NTP地址
+DHCP_NTP="${DHCP_NTP:=ntp.aliyun.com}"
+# PXE 路径
+PXE_PATH="${PXE_PATH:=/srv/tftp}"
+# 系统名称
+OS_NAME="${OS_NAME:=Ubuntu Jammy}"
+# 系统版本
+OS_VER="${OS_VER:=(22.04.3)}"
+# 系统编号
+OS_NUM="${OS_NUM:=ubuntu22043}"
+# ISO 下载地址
+PXE_ISO_URL="${PXE_ISO_URL:=http://${PXE_IP}/get/iso/ubuntu-22.04.3-live-server-amd64.iso}"
+# KS 配置文件下载地址
+PXE_KS_URL="${PXE_KS_URL:=http://${PXE_IP}/get/ks/ubuntu22043/autoinstall/}"
+
+echo "接口名称:" ${INTERFACE}
+echo "PXE IP地址:" ${PXE_IP}
+echo "DHCP起始地址:" ${DHCP_RANGE_LOW}
+echo "DHCP起始结束地址:" ${DHCP_RANGE_HIGH}
+echo "DHCP DNS地址:" ${DHCP_DNS}
+echo "DHCP_NTP:" ${DHCP_NTP}
+echo "PXE 路径:" ${PXE_PATH}
+echo "系统名称:" ${OS_NAME}
+echo "系统版本:" ${OS_VER}
+echo "系统编号:" ${OS_NUM}
+echo "ISO 下载地址:" ${PXE_ISO_URL}
+echo "KS 配置文件下载地址:" ${PXE_KS_URL}
 
 ## 创建相关目录文件
 mkdir -pv ${PXE_PATH}/{bios,uefi,boot,iso,grub,ks,pxelinux.cfg} && chmod -R 775 ${PXE_PATH}
